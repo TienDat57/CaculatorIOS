c12d830f5454343d4bcaa3d37f67b4b79560e7af
TienDat57 <dangtiendat1135@gmail.com>
2022-10-16 17:29:02 +0700

    [UT-01] | set up SSR project using reactJS

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = [
+   {
+      mode: "development",
+      entry: ["@babel/polyfill", "./server/server.js"],
+      output: {
+         path: path.join(__dirname, "./build/"),
+         filename: "server.js",
+         libraryTarget: "commonjs2",
+         publicPath: "/"
+      },
+      target: "node",
+     
+      externals: nodeExternals(),
+      module: {
+         rules: [
+            {
+               test: /\.(js|jsx)$/,
+               exclude: /node_modules/,
+               use: [
+                  {
+                     loader: "babel-loader"
+                  }
+               ]
+            }
+         ]
+      },
+      resolve: {
+         extensions: [".js", ".jsx"]
+      }
+   },
+   {
+      mode: "development",
+      entry: ["@babel/polyfill", "./src/index.js"],
+      output: {
+         path: path.join(__dirname, "./build/"),
+         publicPath: "/",
+         filename: "bundle.js"
+      },
+      module: {
+         rules: [
+            {
+               test: /\.(js|jsx)$/,
+               exclude: /node_modules/,
+               use: [
+                  {
+                     loader: "babel-loader"
+                  }
+               ]
+            }
+         ]
+      },
+      resolve: {
+         extensions: [".js", ".jsx"]
+      },
+      plugins: [
+         new htmlWebpackPlugin({
+            template: "./public/index.html"
+         })
+      ]
+   }
+];
