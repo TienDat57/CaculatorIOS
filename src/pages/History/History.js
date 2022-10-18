2930065a26266e57eb9c881e5fed672735cb4c8d
TienDat57 <dangtiendat1135@gmail.com>
2022-10-19 01:01:37 +0700

    [UT - 04] | add component page History

--- src/pages/History/History.js
+++ src/pages/History/History.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from "react";
 
 function History() {
+   let history = typeof window !== "undefined" ? JSON.parse(localStorage.getItem("listCalculation")) : [];
    return (
-      <div className="history">
+      <div className="listHistory">
          <h1>History</h1>
+         <ul>
+            {history && history.map((item, index) => {
+               return (
+                  <li key={index}>
+                     <span>{item.expression} = {item.result}</span>
+                  </li>
+               )
+            })}
+         </ul>
       </div>
    );
 }
