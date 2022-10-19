466365f4a1d12fc74203d483da71957595fffb67
Tien Dat Dang <84223318+TienDat57@users.noreply.github.com>
2022-10-19 22:16:05 +0700

    [HF - 01] | fix UI history and calculator (#5)

--- src/pages/History/History.js
+++ src/pages/History/History.js
@@ -8,22 +8,22 @@ const cx = classNames.bind(styles);
 function History() {
    let historyStorage = typeof window !== "undefined" ? JSON.parse(localStorage.getItem("listCalculation")) : [];
    return (
-      <div className={cx("listHistory")}>
-         <h1 className={cx("history-title")}>History</h1>
-         <ol className={cx("list-calculation")}>
-            {historyStorage && historyStorage.map((item, index) => {
-               return (
-                  <li key={index} className={cx("history-item")}>
-                     <span>{item.expression} = {item.result}</span>
-                  </li>
-               )
-            })}
-         </ol>
-         <button className={cx("clear-history")} onClick={() => {
-            localStorage.clear();
-            window.location.reload();
-         }}>Clear History</button>
-      </div>
+         <div className={cx("listHistory")}>
+            <h1 className={cx("history-title")}>History</h1>
+            <ol className={cx("list-calculation")}>
+               {historyStorage && historyStorage.map((item, index) => {
+                  return (
+                     <li key={index} className={cx("history-item")}>
+                        <span>{item.expression} = {item.result}</span>
+                     </li>
+                  )
+               })}
+            </ol>
+            <button className={cx("clear-history")} onClick={() => {
+               localStorage.clear();
+               window.location.reload();
+            }}>Clear History</button>
+         </div>
    );
 }
 
