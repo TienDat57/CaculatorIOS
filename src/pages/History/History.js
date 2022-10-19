d085d3537d27f7165e92dbb80319ec63b50b137c
TienDat57 <dangtiendat1135@gmail.com>
2022-10-19 10:24:57 +0700

    [UT - 04] | set styling history

--- src/pages/History/History.js
+++ src/pages/History/History.js
@@ -1,19 +1,28 @@
 import React from "react";
+import classNames from "classnames/bind";
+
+import styles from "./History.module.scss";
+
+const cx = classNames.bind(styles);
 
 function History() {
    let history = typeof window !== "undefined" ? JSON.parse(localStorage.getItem("listCalculation")) : [];
    return (
-      <div className="listHistory">
-         <h1>History</h1>
-         <ul>
+      <div className={cx("listHistory")}>
+         <h1 className={cx("history-title")}>History</h1>
+         <ol className={cx("list-calculation")}>
             {history && history.map((item, index) => {
                return (
-                  <li key={index}>
+                  <li key={index} className={cx("history-item")}>
                      <span>{item.expression} = {item.result}</span>
                   </li>
                )
             })}
-         </ul>
+         </ol>
+         <button className={cx("clear-history")} onClick={() => {
+            localStorage.clear();
+            window.location.reload();
+         }}>Clear History</button>
       </div>
    );
 }
